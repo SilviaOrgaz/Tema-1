3960b4fc7abef3b5dcf2c3347645ec63a8c37da8
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-09-28 22:49:03 +0200

    he creado una primera función

---
+++ Ejercicio5.py
@@ -0,0 +1,41 @@
+#Crea un script llamado descomposicion.py que realice las siguientes tareas:
+#Debe tomar 1 argumento que será un número entero positivo.
+#En caso de no recibir un argumento, debe mostrar información acerca de cómo utilizar el script.
+#El objetivo del script es descomponer el número en unidades, decenas, centenas, miles... tal que por ejemplo si se introduce el número 3647:
+#python descomposicion.py 3647
+#El programa deberá devolver una descomposición línea a línea como la siguiente utilizando las técnicas de formateo:
+#0007
+#0040
+#0600
+#3000
+#Pista
+#Que el valor sea un número no significa necesariamente que deba serlo para formatearlo. 
+#Necesitarás jugar muy bien con los índices y realizar muchas conversiones entre tipos cadenas, números y viceversa
+
+
+
+def detectar_numero_positivo(numero):
+    try:
+        numero = int(numero)
+    except:
+        print("Debes introducir un número")
+        return -1
+    if numero >= 0:
+        print("Genial")
+    else:
+        print("Este número no es positivo")  
+    return numero
+
+detectar_numero=-1
+while detectar_numero < 0:
+    numero = input("Introduce un número positivo: ")
+    detectar_numero = detectar_numero_positivo(numero)
+
+
+
+
+
+
+
+
+
