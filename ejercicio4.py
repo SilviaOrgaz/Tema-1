3b0274d1bedd32f083d3b34b4592b5ac252717ed
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-10-01 10:30:05 +0200

    este ejercicio lo tenia ya hecho

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,18 +1,2 @@
-lista = [4,3,6,1]
-lista_ordenada = sorted(lista)
-print(lista_ordenada)
-lista_pares =[]
-lista_impares = []
 
-for i in lista_ordenada:
-    if i % 2 == 0:
-        lista_par = lista_pares.append(i)
-        print(lista_par)
-        continue
-    elif i % 2!=0:
-        lista_impar = lista_impares.append(i)
-        print(lista_impar)
-        continue
-    else:
-        break 
 
