909d6865f602c262a150715f26909ba4f2b8457c
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-10-01 11:21:48 +0200

    he corregido detalles del ejercicio

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -5,17 +5,18 @@ lista_1 = ["h",'o','l','a',' ', 'm','u','n','d','o']
 
 lista_2 = ["h",'o','l','a',' ', 'l','u','n','a']
 
+#Así recorreria la lista por indices pero no estaría bien programado porque las listas tienen distinta longitud.
 #def lista_repetir(lista_1, lista_2):
     #lista_3 = []
-    #i=0
-    #while lista_1[i] == lista_2[i]:
-        #lista_3.append(i)
-        #i+=1
+    #for i in range(len(lista_2)):
+        #if lista_1[i] == lista_2[i]:
+            #lista_3.append(lista_1[i])
     #return lista_3
 
 #lista_3 = lista_repetir(lista_1, lista_2)
-#print(lista_3)
+#(lista_3)
 
+#Función que dada dos lista devuelve una lista con elementos comunes. Va recorriendo los elementos en vez de las posiciones
 def lista_repetir_2(lista_1, lista_2):
     lista_3=[]
     for i in lista_2:
@@ -23,6 +24,6 @@ def lista_repetir_2(lista_1, lista_2):
             lista_3.append(i)
     return lista_3
 
-lista_3 = lista_repetir_2(lista_1, lista_2)
-print(lista_3)
+lista = lista_repetir_2(lista_1, lista_2)
+print(lista)
 
