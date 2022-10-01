7075fbfd78f9c7611ba6230c974556ae0e1e93ff
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-10-01 10:51:01 +0200

    he terminado el ejericicio

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -9,26 +9,28 @@ from tkinter import N
 #Finalmente muestra el valor final del numero_magico por pantalla
 
 numero_magico = 12345679
-numero_usuario = input("Introduzca un numero entre el 1 y el 9:  ")
-
 
 def mult_numero_usuario(numero_usuario, numero_magico):
     try:
         numero_usuario = int(numero_usuario)
     except:
         print("Esto no es un numero")
-        return False
+        return -1
     if numero_usuario not in range(1,9):
         print("Error no es un numero entre 1 y 9")
-        return False
+        return -1
     else:
         numero_usuario *= 9
         numero_magico *= numero_usuario 
         print(f"El numero final es : {numero_magico}")
         return numero_magico
 
-numero_final = mult_numero_usuario(numero_usuario, numero_magico)
-#print(f"El numero final es : {numero_magico}")
+numero_usuario_2 = -1
+while numero_usuario_2 < 0:
+    numero_usuario = input("Introduzca un numero entre el 1 y el 9:  ")
+    numero_final = mult_numero_usuario(numero_usuario, numero_magico)
+    
+
 
 
 
